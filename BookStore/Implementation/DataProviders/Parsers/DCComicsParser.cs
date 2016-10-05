e6e70c0de05f6d1d05127514fc308053fe7b3f33
Alexander Chicherskiy <chicherskiy@skbkontur.ru>
2016-10-06 01:43:39 +0500

    added xml parsers

---
+++ BookStore/Implementation/DataProviders/Parsers/DCComicsParser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace BookStore.Implementation.DataProviders.Parsers
+{
+    public class DCComicsParser : IProviderXmlParser
+    {
+        public string ProviderName => "DC Comics";
+
+        public bool CanParse(ProviderXmlData providerXmlData)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(providerXmlData.FilePath);
+            return !string.IsNullOrWhiteSpace(fileName) &&
+                   fileName.StartsWith("dc_", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public Book[] Parse(ProviderXmlData providerXmlData)
+        {
+            var booksList = providerXmlData.Data.SelectNodes("/Data/book");
+            if (booksList == null || booksList.Count == 0)
+            {
+                return new Book[0];
+            }
+
+            var books = booksList
+                .Cast<XmlNode>()
+                .Select(ParseBook)
+                .ToArray();
+
+            return books;
+        }
+
+        private Book ParseBook(XmlNode source)
+        {
+            var isbn = ReadNodeText(source, "isbn");
+            var author = ReadNodeText(source, "author");
+            var title = ReadNodeText(source, "title");
+
+            return new Book(isbn, author, title);
+        }
+
+        private string ReadNodeText(XmlNode source, string nodeName)
+        {
+            var node = source.SelectSingleNode(nodeName);
+            if (string.IsNullOrWhiteSpace(node?.Value))
+            {
+                throw new Exception($"Can't parse node \"{nodeName}\" from\r\n{source.OuterXml}");
+            }
+
+            return node.Value;
+        }
+    }
+}
---
+++ BookStore/Implementation/DataProviders/Parsers/MarvelParser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+
+namespace BookStore.Implementation.DataProviders.Parsers
+{
+    public class MarvelParser : IProviderXmlParser
+    {
+        private const string publisherName = "marvel";
+
+        public string ProviderName => "Marvel Comics";
+
+        public bool CanParse(ProviderXmlData providerXmlData)
+        {
+            var publisherNameNode = providerXmlData.Data.SelectSingleNode("/data/publisher/name/text()");
+            if (publisherNameNode == null)
+            {
+                return false;
+            }
+
+            var name = publisherNameNode.Value;
+            return string.Equals(name, publisherName, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public Book[] Parse(ProviderXmlData providerXmlData)
+        {
+            var booksList = providerXmlData.Data.SelectNodes("/data/booklist/book");
+            if (booksList == null || booksList.Count == 0)
+            {
+                return new Book[0];
+            }
+
+            var books = booksList
+                .Cast<XmlNode>()
+                .Select(ParseBook)
+                .ToArray();
+
+            return books;
+        }
+
+        private Book ParseBook(XmlNode source)
+        {
+            var isbn = ReadAttribure(source, "isbn");
+            var author = ReadAttribure(source, "author");
+            var title = ReadAttribure(source, "title");
+
+            return new Book(isbn, author, title);
+        }
+
+        private string ReadAttribure(XmlNode source, string name)
+        {
+            if (source.Attributes == null)
+            {
+                throw new Exception($"Can't parse book from\r\n{source.OuterXml}");
+            }
+
+            var attribute = source.Attributes[name];
+            if (attribute == null)
+            {
+                throw new Exception($"Can't get book attribute from\r\n{source.OuterXml}");
+            }
+
+            var value = attribute.Value;
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new Exception($"Attribute value is empty in node\r\n{source.OuterXml}");
+            }
+
+            return value;
+        }
+    }
+}
