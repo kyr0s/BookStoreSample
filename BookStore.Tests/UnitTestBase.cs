1b07cd24b2f1201c517fbc30d2af810f714ecbd9
Alexander Chicherskiy <chicherskiy@skbkontur.ru>
2016-10-06 01:44:14 +0500

    add test project

---
+++ BookStore.Tests/UnitTestBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace BookStore.Tests
+{
+    [TestFixture]
+    abstract class UnitTestBase
+    {
+        private MockRepository mocks;
+
+        [SetUp]
+        protected virtual void SetUp()
+        {
+            mocks = new MockRepository();
+        }
+
+        protected T NewMock<T>()
+        {
+            return mocks.StrictMock<T>();
+        }
+
+        protected IDisposable MocksRecord()
+        {
+            return mocks.Record();
+        }
+
+        [TearDown]
+        protected virtual void TearDown()
+        {
+            mocks.Record().Dispose();
+            mocks.VerifyAll();
+        }
+    }
+}
