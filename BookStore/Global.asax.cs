a5bd558c74d09d470938443420d14c02bfd2560e
Alexander Chicherskiy <chicherskiy@skbkontur.ru>
2016-10-07 02:03:35 +0500

    logging added

--- BookStore/Global.asax.cs
+++ BookStore/Global.asax.cs
@@ -1,20 +1,31 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using System.Web.Routing;
 using BookStore.Container;
 using BookStore.Implementation;
+using NLog;
 
 namespace BookStore
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private readonly ILogger log = LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             NinjectContainer.RegisterModules(new MainModule());
 
-            var bookSearchServcie = NinjectContainer.Resolve<IBookIndexBuilder>();
-            bookSearchServcie.Build();
+            var bookIndexBuilder = NinjectContainer.Resolve<IBookIndexBuilder>();
+            bookIndexBuilder.Build();
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+            log.Error(exception);
+        }
+
     }
 }
